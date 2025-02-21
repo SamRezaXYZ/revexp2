2ca1fd6e028a2cfc131f78fc05aee6c963f973ab
SamReza <samrezaxyz@gmail.com>
2025-02-21 14:09:01 +0600

    Update DisableAdsPatch.kt

--- patches/src/main/kotlin/li/auna/patches/telegram/ads/DisableAdsPatch.kt
+++ patches/src/main/kotlin/li/auna/patches/telegram/ads/DisableAdsPatch.kt
@@ -11,7 +11,8 @@ val hideAdsPatch = bytecodePatch(
     compatibleWith(
         "org.telegram.messenger",
         "org.telegram.messenger.web",
-        "uz.unnarsx.cherrygram"
+        "uz.unnarsx.cherrygram",
+        "com.iMe.android.web"
     )
 
     execute {
